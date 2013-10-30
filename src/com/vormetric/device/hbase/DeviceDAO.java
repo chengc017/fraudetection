3c708de6f3120e0a13237dcff661ded38cb4c1ca
xioguo <xioguo@ebay.com>
2013-10-30 14:07:35 +0800

    convert HBase Result into DeviceModel

--- src/com/vormetric/device/hbase/DeviceDAO.java
+++ src/com/vormetric/device/hbase/DeviceDAO.java
@@ -6,6 +6,9 @@ package com.vormetric.device.hbase;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.Set;
 
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
@@ -19,8 +22,9 @@ import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
 
-import com.vormetric.device.model.DeviceDTO;
 import com.vormetric.device.model.DeviceModel;
+import com.vormetric.device.utils.AttibutesConvertor;
+import com.vormetric.device.utils.Md5Utils;
 
 /**
  * @author xioguo
@@ -39,8 +43,6 @@ public class DeviceDAO {
 	public static final byte[] TRANSACTION_FAMILY = Bytes.toBytes("t");
 	public static final byte[] TRANSACTION_COL = Bytes.toBytes("");
 	
-	private static final int longLength = 8;
-	
 	private HBaseAdmin admin;
 	
 	private static final Logger log = Logger.getLogger(DeviceDAO.class);
@@ -49,12 +51,17 @@ public class DeviceDAO {
 		this.admin = admin;
 	}
 	
-	public static byte [] mkRowKey(DeviceModel d) {
-		return mkRowKey(d.getBrowserHash(), System.currentTimeMillis());
+	private static byte [] mkRowKey(DeviceModel d) {
+		//create unique row key using request id & timestamp
+		return mkRowKey(d.getRequestId(), System.currentTimeMillis());
 	}
 	
-	public static byte[] mkRowKey(String hash, long dt) {
-		byte[] browserHash = Bytes.toBytes(hash);
+	/*
+	 * if you want to create a unique row key on your own, 
+	 * pass a string and timemillis.
+	 * */
+	public static byte[] mkRowKey(String str, long dt) {
+		byte[] browserHash = Md5Utils.md5sum(str);
 		byte[] timestamp = Bytes.toBytes(-1*dt);
 		byte[] rowKey = new byte[browserHash.length+timestamp.length];
 		
@@ -65,6 +72,7 @@ public class DeviceDAO {
 	}
 	
 	public Put mkPut(DeviceModel d) {
+		//create unique row key using default method
 		return mkPut(d, mkRowKey(d), 1, 0, 0);
 	}
 	
@@ -78,30 +86,49 @@ public class DeviceDAO {
 	}
 	
 	public Get mkGet(String hash) {
+		//TODO
 		return null;
 	}
 	
-	
-	public List<DeviceDTO> list(String hash) throws IOException {
-		List<DeviceDTO> results = new ArrayList<DeviceDTO>();
+	public List<DeviceModel> list(String hash) throws IOException {
+		List<DeviceModel> results = new ArrayList<DeviceModel>();
 		HTable t = new HTable(admin.getConfiguration(), TABLE_NAME);
-		
 		Scan scan = new Scan();
 		scan.addColumn(DeviceDAO.HASH_FAMILY, DeviceDAO.HASH_COL);
 		scan.addColumn(DeviceDAO.LENGTH_FAMILY, DeviceDAO.LENGTH_COL);
 		scan.addFamily(DeviceDAO.DEVICE_FAMILY);
-		
 		scan.setFilter(new SingleColumnValueFilter(HASH_FAMILY, HASH_COL,
 				CompareOp.EQUAL, Bytes.toBytes(hash)));
 		
 		ResultScanner rs = t.getScanner(scan);
-		int i=0;
 		for (Result r : rs) {
-			results.add(new DeviceDTO(r));
-			i++;
+			results.addAll(convert(r));
 		}
 		rs.close();
 		t.close();
 		return results;
+	}
+	
+	private List<DeviceModel> convert(Result r) {
+		List<DeviceModel> deviceList = new ArrayList<DeviceModel> ();
+		NavigableMap<byte[], byte[]> dmap = r.getFamilyMap(DeviceDAO.DEVICE_FAMILY);
+		Set<Entry<byte[], byte[]>> entrySet = dmap.entrySet();
+		for(Entry<byte[], byte[]> entry: entrySet) {
+			DeviceModel device = new DeviceModel();
+			device.setRowKey(r.getRow());
+			device.setBrowserHash(Bytes.toString(r.getColumnLatest(
+					DeviceDAO.HASH_FAMILY, DeviceDAO.HASH_COL).getValue()));
+			
+			AttibutesConvertor convertor = new AttibutesConvertor(
+					Bytes.toString(entry.getValue()));
+			if(convertor.isValid()) {
+				device.setBrowserAttributes(convertor.getBrowserAtts());
+				device.setPluginAttributes(convertor.getPluginAtts());
+				device.setOsAttributes(convertor.getOSAtts());
+				device.setConnectionAttributes(convertor.getConnAtts());
+			}
+			deviceList.add(device);
+		}
+		return deviceList;
 	}
 }
